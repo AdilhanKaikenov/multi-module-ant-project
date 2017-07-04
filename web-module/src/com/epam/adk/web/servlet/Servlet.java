80e3816fe88d25a7683b676e434e9a7992b2aae9
Adilkhan_Kaikenov <adilhan_kai@mail.ru>
2017-07-04 14:50:20 +0600

    Servlet.java & UI added

--- web-module/src/com/epam/adk/web/servlet/Servlet.java
+++ web-module/src/com/epam/adk/web/servlet/Servlet.java
@@ -1,5 +1,9 @@
 package com.epam.adk.web.servlet;
 
+import com.epam.adk.ejb.dao.MessageDao;
+import com.epam.adk.ejb.persistance.Message;
+
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,13 +19,25 @@ import java.io.IOException;
  */
 public class Servlet extends HttpServlet {
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    @EJB
+    private MessageDao messageDao;
 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("In the Servlet doPost() method: ");
+
+        Message message = new Message();
+        message.setText(request.getParameter("message"));
+        messageDao.submit(message);
+
+        System.out.println(message.getText());
+        System.out.println("ID: " + message.getId());
+
+        request.setAttribute("message", message.getText());
+
+        request.getRequestDispatcher("/WEB-INF/welcome.jsp").forward(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("In the Servlet doGet() method: ");
-
-        request.getRequestDispatcher("/index.jsp").forward(request, response);
     }
 }
--- web-module/web/index.jsp
+++ web-module/web/index.jsp
@@ -1,9 +1,18 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
 <html>
 <head>
     <title>Index</title>
 </head>
 <body>
-    <h2>Hello World!</h2>
+
+<form action="${pageContext.request.contextPath}/do/?page=welcome" method="post">
+    <label>
+        <input name="message" type="text">
+    </label>
+    <br>
+    <input name="submit" type="submit" value="Send message">
+</form>
+
 </body>
 </html>
